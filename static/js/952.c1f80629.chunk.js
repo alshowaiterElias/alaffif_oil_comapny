8c370bc98c8c1ed7bc51e347822cb9f446d7905e
alshowaiterElias <elias.alshowaiter@neurobazar.com>
2025-04-26 12:31:06 +0300

    Updates

---
+++ static/js/952.c1f80629.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkadmin_panel=self.webpackChunkadmin_panel||[]).push([[952],{4952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.c1f80629.chunk.js.map
